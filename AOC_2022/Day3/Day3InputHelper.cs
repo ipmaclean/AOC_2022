ede8cb306b461cfb20963463ff47d5bc3609d64d
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2022-12-01 08:38:13 +0000

    Solve Day One. (#1)

---
+++ AOC_2022/Day3/Day3InputHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace AOC_2022.Day3
+{
+    internal class Day3InputHelper : InputHelper<List<int>>
+    {
+        public Day3InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<int> Parse()
+        {
+            var output = new List<int>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                }
+            }
+            return output;
+        }
+    }
+}
