490d68c196e87bfecbe2b470320061745208173a
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2022-12-05 08:47:03 +0000

    Solved Day Five (#7)

--- AOC_2022/Day5/Day5InputHelper.cs
+++ AOC_2022/Day5/Day5InputHelper.cs
@@ -1,22 +1,56 @@
-﻿namespace AOC_2022.Day5
+﻿using System.Text.RegularExpressions;
+
+namespace AOC_2022.Day5
 {
-    internal class Day5InputHelper : InputHelper<List<int>>
+    internal class Day5InputHelper
+        : InputHelper<(List<Stack<char>>, List<List<char>>, List<(int Amount, int From, int To)>)>
     {
         public Day5InputHelper(string fileName) : base(fileName)
         {
         }
 
-        public override List<int> Parse()
+        public override (List<Stack<char>>, List<List<char>>, List<(int Amount, int From, int To)>) Parse()
         {
-            var output = new List<int>();
+            var tempStacks = new List<List<char>>();
+            var instructions = new List<(int, int, int)>();
             using (var sr = new StreamReader(InputPath))
             {
                 string ln;
+                var numberRegex = new Regex(@"\d+");
+                while ((ln = sr.ReadLine()!) != String.Empty)
+                {
+                    if (ln[1] == '1')
+                    {
+                        continue;
+                    }
+                    var stackNumber = 0;
+                    for (var i = 1; i < ln.Length; i += 4)
+                    {
+                        if (tempStacks.Count < stackNumber + 1)
+                        {
+                            tempStacks.Add(new List<char>());
+                        }
+                        if (ln[i] != ' ')
+                        {
+                            tempStacks[stackNumber].Add(ln[i]);
+                        }
+                        stackNumber++;
+                    }
+                }
+
                 while ((ln = sr.ReadLine()!) != null)
                 {
+                    var matches = numberRegex.Matches(ln);
+                    instructions.Add((int.Parse(matches[0].Value), int.Parse(matches[1].Value) - 1, int.Parse(matches[2].Value) - 1));
                 }
             }
-            return output;
+            var stacks = new List<Stack<char>>();
+            foreach (var tempStack in tempStacks)
+            {
+                stacks.Add(new Stack<char>(tempStack));
+                tempStack.Reverse();
+            }
+            return (stacks, tempStacks, instructions);
         }
     }
 }
--- AOC_2022/Day5/Day5PuzzleManager.cs
+++ AOC_2022/Day5/Day5PuzzleManager.cs
@@ -2,9 +2,13 @@
 {
     public class Day5PuzzleManager : PuzzleManager
     {
+        public List<Stack<char>> Stacks { get; set; }
+        public List<List<char>> Lists { get; set; }
+        public List<(int Amount, int From, int To)> Instructions { get; set; }
         public Day5PuzzleManager()
         {
             var inputHelper = new Day5InputHelper(INPUT_FILE_NAME);
+            (Stacks, Lists, Instructions) = inputHelper.Parse();
         }
         public override Task SolveBothParts()
         {
@@ -16,16 +20,63 @@
 
         public override Task SolvePartOne()
         {
-            var solution = 0;
+            var solution = string.Empty;
+            var stacks = CloneStacks();
+            foreach (var instruction in Instructions)
+            {
+                for (var i = 0; i < instruction.Amount; i++)
+                {
+                    stacks[instruction.To].Push(
+                        stacks[instruction.From].Pop()
+                        );
+                }
+            }
+            foreach (var stack in stacks)
+            {
+                solution += stack.Peek();
+            }
             Console.WriteLine($"The solution to part one is '{solution}'.");
             return Task.CompletedTask;
         }
 
         public override Task SolvePartTwo()
         {
-            var solution = 0;
+            var solution = string.Empty;
+            var lists = CloneLists();
+            foreach (var instruction in Instructions)
+            {
+                lists[instruction.To].AddRange(
+                    lists[instruction.From].GetRange(
+                        lists[instruction.From].Count - instruction.Amount, instruction.Amount
+                        ));
+                lists[instruction.From].RemoveRange(lists[instruction.From].Count - instruction.Amount, instruction.Amount);
+            }
+            foreach (var list in lists)
+            {
+                solution += list.Last();
+            }
             Console.WriteLine($"The solution to part two is '{solution}'.");
             return Task.CompletedTask;
+        }
+
+        private List<Stack<char>> CloneStacks()
+        {
+            var stacks = new List<Stack<char>>();
+            foreach (var stack in Stacks)
+            {
+                stacks.Add(new Stack<char>(stack));
+            }
+            return stacks;
+        }
+
+        private List<List<char>> CloneLists()
+        {
+            var lists = new List<List<char>>();
+            foreach (var list in Lists)
+            {
+                lists.Add(new List<char>(list));
+            }
+            return lists;
         }
     }
 }
