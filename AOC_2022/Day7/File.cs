20ff985c8cd99ece536983562a03fca125500e93
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2022-12-07 08:35:39 +0000

    Solved Day Seven (#9)

---
+++ AOC_2022/Day7/File.cs
@@ -0,0 +1,16 @@
+﻿namespace AOC_2022.Day7
+{
+    public class File
+    {
+        public File(string name, long size, Folder parent)
+        {
+            Name = name;
+            Size = size;
+            Parent = parent;
+        }
+
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public Folder Parent { get; set; }
+    }
+}
---
+++ AOC_2022/Day7/Folder.cs
@@ -0,0 +1,18 @@
+﻿namespace AOC_2022.Day7
+{
+    public class Folder
+    {
+        public Folder(string name, Folder? parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+
+        public string Name { get; set; }
+        public long? Size { get; set; }
+        public Folder? Parent { get; set; }
+
+        public ICollection<Folder> Folders { get; set; } = new List<Folder>();
+        public ICollection<File> Files { get; set; } = new HashSet<File>();
+    }
+}
