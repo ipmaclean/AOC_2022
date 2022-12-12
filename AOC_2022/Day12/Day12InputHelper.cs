72b799a823570ab39f5ee4ad34855e44614fc5c6
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-12-12 09:02:36 +0000

    Solved Day Twelve.

--- AOC_2022/Day12/Day12InputHelper.cs
+++ AOC_2022/Day12/Day12InputHelper.cs
@@ -1,19 +1,39 @@
 ﻿namespace AOC_2022.Day12
 {
-    internal class Day12InputHelper : InputHelper<List<int>>
+    internal class Day12InputHelper : InputHelper<Tile[,]>
     {
         public Day12InputHelper(string fileName) : base(fileName)
         {
         }
 
-        public override List<int> Parse()
+        public override Tile[,] Parse()
         {
-            var output = new List<int>();
+            var output = new Tile[0, 0];
+            var yCount = 0;
+            var xCount = 0;
             using (var sr = new StreamReader(InputPath))
             {
                 string ln;
                 while ((ln = sr.ReadLine()!) != null)
                 {
+                    xCount = ln.Length;
+                    yCount++;
+                }
+            }
+
+            using (var sr = new StreamReader(InputPath))
+            {
+                output = new Tile[xCount, yCount];
+                var yCoord = 0;
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var xCoord = 0;
+                    foreach (var tile in ln)
+                    {
+                        output[xCoord, yCoord] = new Tile(tile, (xCoord++, yCoord));
+                    }
+                    yCoord++;
                 }
             }
             return output;
---
+++ AOC_2022/Day14/Day14InputHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace AOC_2022.Day14
+{
+    internal class Day14InputHelper : InputHelper<List<int>>
+    {
+        public Day14InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<int> Parse()
+        {
+            var output = new List<int>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                }
+            }
+            return output;
+        }
+    }
+}
