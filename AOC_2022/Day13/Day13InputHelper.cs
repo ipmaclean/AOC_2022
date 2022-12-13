f9de4a5d76347564fddd10193efb9d84b3199239
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-12-13 08:46:56 +0000

    Solved Part Two.

--- AOC_2022/Day13/Day13InputHelper.cs
+++ AOC_2022/Day13/Day13InputHelper.cs
@@ -3,23 +3,24 @@ using Newtonsoft.Json.Linq;
 
 namespace AOC_2022.Day13
 {
-    internal class Day13InputHelper : InputHelper<List<(JArray left, JArray right)>>
+    internal class Day13InputHelper : InputHelper<List<Packet>>
     {
         public Day13InputHelper(string fileName) : base(fileName)
         {
         }
 
-        public override List<(JArray left, JArray right)> Parse()
+        public override List<Packet> Parse()
         {
-            var output = new List<(JArray left, JArray right)>();
+            var output = new List<Packet> ();
             using (var sr = new StreamReader(InputPath))
             {
                 string ln;
                 do
                 {
                     var left = JArray.Parse(sr.ReadLine()!);
+                    output.Add(new Packet(left));
                     var right = JArray.Parse(sr.ReadLine()!);
-                    output.Add((left, right));
+                    output.Add(new Packet(right));
                 }
                 while ((ln = sr.ReadLine()!) != null);
             }
--- AOC_2022/Day13/Day13PuzzleManager.cs
+++ AOC_2022/Day13/Day13PuzzleManager.cs
@@ -4,13 +4,11 @@ namespace AOC_2022.Day13
 {
     public class Day13PuzzleManager : PuzzleManager
     {
-        //new protected const string INPUT_FILE_NAME = "test.txt";
+        private Day13InputHelper _inputHelper;
 
-        public List<(JArray left, JArray right)> Input { get; set; }
         public Day13PuzzleManager()
         {
-            var inputHelper = new Day13InputHelper(INPUT_FILE_NAME);
-            Input = inputHelper.Parse();
+            _inputHelper = new Day13InputHelper(INPUT_FILE_NAME);
         }
         public override Task SolveBothParts()
         {
@@ -22,92 +20,33 @@ namespace AOC_2022.Day13
 
         public override Task SolvePartOne()
         {
+            var input = _inputHelper.Parse();
             var solution = 0;
-            for (var i = 0; i < Input.Count; i++)
+            for (var i = 0; i < input.Count; i += 2)
             {
-                var inCorrectOrder = CompareArrays(Input[i].left, Input[i].right);
-                if (!inCorrectOrder.HasValue)
+                var order = input[i].CompareTo(input[i + 1]);
+                if (order == 0)
                 {
                     throw new Exception("Could not identify order of arrays.");
                 }
-                if (inCorrectOrder.Value)
+                if (order == -1)
                 {
-                    solution += i + 1;
+                    solution += (i / 2) + 1;
                 }
             }
             Console.WriteLine($"The solution to part one is '{solution}'.");
             return Task.CompletedTask;
         }
 
-        private bool? CompareArrays(JArray left, JArray right)
-        {
-            for (var i = 0; i < left.Count; i++)
-            {
-                if (right.Count <= i)
-                {
-                    return false;
-                }
-                // Case 1
-                if (left[i].Type == JTokenType.Integer &&
-                    right[i].Type == JTokenType.Integer)
-                {
-                    if ((int)left[i] == (int)right[i])
-                    {
-                        continue;
-                    }
-                    return (int)left[i] < (int)right[i];
-                }
-                // Case 2
-                if (left[i].Type == JTokenType.Array &&
-                    right[i].Type == JTokenType.Array)
-                {
-                    var comparison = CompareArrays((JArray)left[i], (JArray)right[i]);
-                    if (!comparison.HasValue)
-                    {
-                        continue;
-                    }
-                    return comparison.Value;
-                }
-                // Case 3
-                else
-                {
-                    JArray leftArray;
-                    JArray rightArray;
-                    if (left[i].Type == JTokenType.Array)
-                    {
-                        leftArray = (JArray)left[i];
-                    }
-                    else
-                    {
-                        leftArray = new JArray((int)left[i]);
-                    }
-                    if (right[i].Type == JTokenType.Array)
-                    {
-                        rightArray = (JArray)right[i];
-                    }
-                    else
-                    {
-                        rightArray = new JArray((int)right[i]);
-
-                    }
-                    var comparison = CompareArrays(leftArray, rightArray);
-                    if (!comparison.HasValue)
-                    {
-                        continue;
-                    }
-                    return comparison.Value;
-                }
-            }
-            if (right.Count > left.Count)
-            {
-                return true;
-            }
-            return null;
-        }
-
         public override Task SolvePartTwo()
         {
-            var solution = 0;
+            var input = _inputHelper.Parse();
+            var divider1 = new Packet(JArray.Parse(@"[[2]]"));
+            var divider2 = new Packet(JArray.Parse(@"[[6]]"));
+            input.Add(divider1);
+            input.Add(divider2);
+            input.Sort();
+            var solution = (input.IndexOf(divider1) + 1) * (input.IndexOf(divider2) + 1);
             Console.WriteLine($"The solution to part two is '{solution}'.");
             return Task.CompletedTask;
         }
