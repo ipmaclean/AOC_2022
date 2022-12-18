7f2ad4034aaf0715dfbfb48171314f8d275ef0e3
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-12-18 15:25:08 +0000

    Tidy up and added some comments.

--- AOC_2022/Day16/Day16PuzzleManager.cs
+++ AOC_2022/Day16/Day16PuzzleManager.cs
@@ -29,12 +29,6 @@ namespace AOC_2022.Day16
 
         public override Task SolvePartOne()
         {
-            //  Ideas
-            //      Map out shortest distances between (>0) valves and from start
-            //      Pressure released = value * time remaining to avoid tracking things
-            //      Go from highest pressure released over time from current position to lowest?
-            //          That does not give the optimal solution unfortunately
-
             var valves = _inputHelper.Parse();
 
             foreach (var valve in valves.Where(x => x.Name == "AA" || x.Rate > 0))
@@ -59,7 +53,7 @@ namespace AOC_2022.Day16
 
                 foreach (var nextValve in workingValves.Where(x => !currentState.Path.Contains(x.Name)))
                 {
-                    // Time's up
+                    // You have no time to visit the next valve - we have a solution
                     if (currentState.TimeRemaining - (currentState.Valve.Distances[nextValve] + 1) <= 0)
                     {
                         solution = Math.Max(
@@ -72,7 +66,7 @@ namespace AOC_2022.Day16
                     var newTotalPressureReleased =
                         currentState.TotalPressureReleased + (currentState.TimeRemaining - (currentState.Valve.Distances[nextValve] + 1)) * nextValve.Rate;
 
-                    // Visited every working valve
+                    // Visited every working valve - we have a solution
                     if (currentState.Path.Count + 1 == workingValves.Count)
                     {
                         solution = Math.Max(
@@ -95,6 +89,117 @@ namespace AOC_2022.Day16
             }
 
             Console.WriteLine($"The solution to part one is '{solution}'.");
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartTwo()
+        {
+            var valves = _inputHelper.Parse();
+            var opti1 = 0L;
+            var opti2 = 0L;
+
+            foreach (var valve in valves.Where(x => x.Name == "AA" || x.Rate > 0))
+            {
+                FindShortestDistances(valve, valves);
+                ResetShortestPaths(valves);
+            }
+
+            var solution = 0;
+            var firstValve = valves.First(x => x.Name == "AA");
+            var workingValves = valves.Where(x => x.Rate > 0).ToList();
+            var visitedStates = new HashSet<string>();
+
+            // We will use the lowest time remaining at the priority in the queue to avoid
+            // the queue being too long at any point in time.
+            var nextSteps = new PriorityQueue<Day16BreadthFirstSearchState[], int>();
+            nextSteps.Enqueue(
+                new Day16BreadthFirstSearchState[] {
+                    new Day16BreadthFirstSearchState(firstValve, new List<string>(), 26, 0),
+                    new Day16BreadthFirstSearchState(firstValve, new List<string>(), 26, 0) },
+                26);
+            while (nextSteps.TryDequeue(out var currentState, out int timeElapsed))
+            {
+                var unvisitedWorkingValves = workingValves.Where(x => !currentState[0].Path.Contains(x.Name) && !currentState[1].Path.Contains(x.Name)).ToList();
+
+                // Move whoever has the most time remaining - you or the elephant
+                var movingState = currentState.First(x => x.TimeRemaining == currentState.Max(x => x.TimeRemaining));
+                var otherState = currentState.First(x => x != movingState);
+
+                // Optimisation - if all remaining closed valve rates * time isn't better than best current, discard this path
+                if (currentState[0].TotalPressureReleased + currentState[1].TotalPressureReleased + workingValves.Where(x => !currentState[0].Path.Contains(x.Name) && !currentState[1].Path.Contains(x.Name)).Sum(x => x.Rate * movingState.TimeRemaining) < solution)
+                {
+                    opti1++;
+                    continue;
+                }
+
+                var addedCurrentState = false;
+                foreach (var nextValve in unvisitedWorkingValves)
+                {
+                    Day16BreadthFirstSearchState[] nextDualState;
+                    // The moving valve closer has no time to visit the next valve
+                    if (movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1) <= 0)
+                    {
+                        // Both you and the elephant have no time to visit anywhere - we have a solution.
+                        if (otherState.TimeRemaining == 0)
+                        {
+                            solution = Math.Max(
+                                solution,
+                                movingState.TotalPressureReleased + otherState.TotalPressureReleased
+                                );
+                            continue;
+                        }
+                        // To avoid adding the same thing to the queue multiple times.
+                        if (addedCurrentState)
+                        {
+                            continue;
+                        }
+                        addedCurrentState = true;
+
+                        // Queue the same state, but with no time remaining.
+                        nextDualState = new Day16BreadthFirstSearchState[]
+                        {
+                            new Day16BreadthFirstSearchState(movingState.Valve, new List<string>(movingState.Path), 0, movingState.TotalPressureReleased),
+                            otherState
+                        };
+                        nextSteps.Enqueue(nextDualState, otherState.TimeRemaining);
+                        continue;
+                    }
+
+                    var newTotalPressureReleased =
+                        movingState.TotalPressureReleased + (movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1)) * nextValve.Rate;
+
+                    // Visited every working valve - we have a solution.
+                    if (movingState.Path.Count + otherState.Path.Count + 1 == workingValves.Count)
+                    {
+                        solution = Math.Max(
+                            solution,
+                            newTotalPressureReleased + otherState.TotalPressureReleased
+                            );
+                        continue;
+                    }
+
+                    var newPath = new List<string>(movingState.Path);
+                    newPath.Add(nextValve.Name);
+
+                    var lowestTimeRemaining = Math.Min(movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1), otherState.TimeRemaining);
+
+                    nextDualState = new Day16BreadthFirstSearchState[]
+                    {
+                        new Day16BreadthFirstSearchState(nextValve, newPath, movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1), newTotalPressureReleased),
+                        otherState
+                    };
+                    var nextDualStateHash = GetDualStateHash(nextDualState);
+                    if (visitedStates.Contains(nextDualStateHash))
+                    {
+                        // Saves about 10s on my input but at the cost of 1.4GB of memory!
+                        opti2++;
+                        continue;
+                    }
+                    visitedStates.Add(nextDualStateHash);
+                    nextSteps.Enqueue(nextDualState, lowestTimeRemaining);
+                }
+            }
+            Console.WriteLine($"The solution to part two is '{solution}'.");
             return Task.CompletedTask;
         }
 
@@ -126,111 +231,6 @@ namespace AOC_2022.Day16
             {
                 valve.ShortestPath = -1;
             }
-        }
-
-        public override Task SolvePartTwo()
-        {
-            var valves = _inputHelper.Parse();
-
-            foreach (var valve in valves.Where(x => x.Name == "AA" || x.Rate > 0))
-            {
-                FindShortestDistances(valve, valves);
-                ResetShortestPaths(valves);
-            }
-
-            var solution = 0;
-            var firstValve = valves.First(x => x.Name == "AA");
-            var workingValves = valves.Where(x => x.Rate > 0).ToList();
-            var visitedStates = new HashSet<string>();
-
-            // Need to use time elapsed for this instead of time left
-            var nextSteps = new PriorityQueue<Day16BreadthFirstSearchState[], int>();
-            nextSteps.Enqueue(
-                new Day16BreadthFirstSearchState[] {
-                    new Day16BreadthFirstSearchState(firstValve, new List<string>(), 26, 0),
-                    new Day16BreadthFirstSearchState(firstValve, new List<string>(), 26, 0) },
-                26);
-            while (nextSteps.TryDequeue(out var currentState, out int timeElapsed))
-            {
-                var unvisitedWorkingValves = workingValves.Where(x => !currentState[0].Path.Contains(x.Name) && !currentState[1].Path.Contains(x.Name)).ToList();
-
-                var movingState = currentState.First(x => x.TimeRemaining == currentState.Max(x => x.TimeRemaining));
-                var otherState = currentState.First(x => x != movingState);
-
-                // Optimisation - if all remaining closed valve rates * time isn't better than best current, discard this path
-                if (currentState[0].TotalPressureReleased + currentState[1].TotalPressureReleased + workingValves.Where(x => !currentState[0].Path.Contains(x.Name) && !currentState[1].Path.Contains(x.Name)).Sum(x => x.Rate * movingState.TimeRemaining) < solution)
-                {
-                    continue;
-                }
-
-
-                var addedCurrentState = false;
-                foreach (var nextValve in unvisitedWorkingValves)
-                {
-                    Day16BreadthFirstSearchState[] nextDualState;
-                    int lowestTimeRemaining;
-                    // Time's up
-                    if (movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1) <= 0)
-                    {
-                        if (otherState.TimeRemaining == 0)
-                        {
-                            solution = Math.Max(
-                                solution,
-                                movingState.TotalPressureReleased + otherState.TotalPressureReleased
-                                );
-                            continue;
-                        }
-                        if (addedCurrentState)
-                        {
-                            continue;
-                        }
-
-                        addedCurrentState = true;
-
-                        nextDualState = new Day16BreadthFirstSearchState[]
-                        {
-                            new Day16BreadthFirstSearchState(movingState.Valve, new List<string>(movingState.Path), 0, movingState.TotalPressureReleased),
-                            otherState
-                        };
-                        lowestTimeRemaining = otherState.TimeRemaining;
-                        nextSteps.Enqueue(nextDualState, lowestTimeRemaining);
-                        continue;
-                    }
-
-                    var newTotalPressureReleased =
-                        movingState.TotalPressureReleased + (movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1)) * nextValve.Rate;
-
-                    // Visited every working valve
-                    if (movingState.Path.Count + otherState.Path.Count + 1 == workingValves.Count)
-                    {
-                        solution = Math.Max(
-                            solution,
-                            newTotalPressureReleased + otherState.TotalPressureReleased
-                            );
-                        continue;
-                    }
-
-                    var newPath = new List<string>(movingState.Path);
-                    newPath.Add(nextValve.Name);
-
-                    lowestTimeRemaining = Math.Min(movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1), otherState.TimeRemaining);
-
-                    nextDualState = new Day16BreadthFirstSearchState[]
-                    {
-                        new Day16BreadthFirstSearchState(nextValve, newPath, movingState.TimeRemaining - (movingState.Valve.Distances[nextValve] + 1), newTotalPressureReleased),
-                        otherState
-                    };
-                    var nextDualStateHash = GetDualStateHash(nextDualState);
-                    if (visitedStates.Contains(nextDualStateHash))
-                    {
-                        continue;
-                    }
-                    visitedStates.Add(nextDualStateHash);
-                    nextSteps.Enqueue(nextDualState, lowestTimeRemaining);
-                }
-            }
-            Console.WriteLine($"The solution to part two is '{solution}'.");
-            return Task.CompletedTask;
         }
 
         private string GetDualStateHash(Day16BreadthFirstSearchState[] dualStates)
