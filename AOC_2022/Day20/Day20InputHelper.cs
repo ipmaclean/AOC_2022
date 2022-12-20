f287b63f622bc0b8a52c51c3f9cf7808bb136b99
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-12-20 09:06:25 +0000

    Solved Part One.

--- AOC_2022/Day20/Day20InputHelper.cs
+++ AOC_2022/Day20/Day20InputHelper.cs
@@ -1,19 +1,21 @@
 ﻿namespace AOC_2022.Day20
 {
-    internal class Day20InputHelper : InputHelper<List<int>>
+    internal class Day20InputHelper : InputHelper<List<WrappedInt>>
     {
         public Day20InputHelper(string fileName) : base(fileName)
         {
         }
 
-        public override List<int> Parse()
+        public override List<WrappedInt> Parse()
         {
-            var output = new List<int>();
+            var output = new List<WrappedInt>();
             using (var sr = new StreamReader(InputPath))
             {
                 string ln;
+                var index = 0;
                 while ((ln = sr.ReadLine()!) != null)
                 {
+                    output.Add(new WrappedInt(long.Parse(ln), index++));
                 }
             }
             return output;
---
+++ AOC_2022/Day20/WrappedInt.cs
@@ -0,0 +1,14 @@
+﻿namespace AOC_2022.Day20
+{
+    public class WrappedInt
+    {
+        public WrappedInt(long value, long originalIndex)
+        {
+            Value = value;
+            OriginalIndex = originalIndex;
+        }
+
+        public long Value { get; set; }
+        public long OriginalIndex { get; set; }
+    }
+}
