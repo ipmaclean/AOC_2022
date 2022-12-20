d9793297b77056f397285ae435b6fa467b6011a1
Iain Maclean <iain.p.m.maclean@gmail.com>
2022-12-20 09:13:44 +0000

    Solved Part Two.

--- AOC_2022/Day20/Day20PuzzleManager.cs
+++ AOC_2022/Day20/Day20PuzzleManager.cs
@@ -2,7 +2,6 @@
 {
     public class Day20PuzzleManager : PuzzleManager
     {
-        //new protected const string INPUT_FILE_NAME = "test.txt";
         private Day20InputHelper _inputHelper;
 
         public Day20PuzzleManager()
@@ -22,27 +21,9 @@
         public override Task SolvePartOne()
         {
             var numbers = _inputHelper.Parse();
-            var count = numbers.Count;
-            for (var i = 0; i < count; i++)
-            {
-                var numberToMove = numbers.First(x => x.OriginalIndex == i);
-                var currentIndex = numbers.IndexOf(numberToMove);
-                numbers.Remove(numberToMove);
-                var newIndex = Mod(currentIndex + numberToMove.Value, numbers.Count);
-                numbers.Insert((int)newIndex, numberToMove);
-            }
+            ReorderListOnePass(numbers, numbers.Count);
 
-            var zero = numbers.First(x => x.Value == 0);
-            var indexOfZero = numbers.IndexOf(zero);
-            var solution = 0L;
-
-            for (var i = 0; i < 3; i++)
-            {
-                indexOfZero = (indexOfZero + 1000) % count;
-                solution += numbers[indexOfZero].Value;
-            }
-
-            Console.WriteLine($"The solution to part one is '{solution}'.");
+            Console.WriteLine($"The solution to part one is '{GetSolution(numbers)}'.");
             return Task.CompletedTask;
         }
 
@@ -50,7 +31,17 @@
         {
             var numbers = _inputHelper.Parse();
             numbers = numbers.Select(x => new WrappedInt(x.Value * 811_589_153, x.OriginalIndex)).ToList();
-            var count = numbers.Count;
+            for (var j = 0; j < 10; j++)
+            {
+                ReorderListOnePass(numbers, numbers.Count);
+            }
+
+            Console.WriteLine($"The solution to part two is '{GetSolution(numbers)}'.");
+            return Task.CompletedTask;
+        }
+
+        private static void ReorderListOnePass(List<WrappedInt> numbers, int count)
+        {
             for (var i = 0; i < count; i++)
             {
                 var numberToMove = numbers.First(x => x.OriginalIndex == i);
@@ -59,19 +50,21 @@
                 var newIndex = Mod(currentIndex + numberToMove.Value, numbers.Count);
                 numbers.Insert((int)newIndex, numberToMove);
             }
+        }
 
+        private static long GetSolution(List<WrappedInt> numbers)
+        {
             var zero = numbers.First(x => x.Value == 0);
             var indexOfZero = numbers.IndexOf(zero);
             var solution = 0L;
 
             for (var i = 0; i < 3; i++)
             {
-                indexOfZero = (indexOfZero + 1000) % count;
+                indexOfZero = (indexOfZero + 1000) % numbers.Count;
                 solution += numbers[indexOfZero].Value;
             }
 
-            Console.WriteLine($"The solution to part two is '{solution}'.");
-            return Task.CompletedTask;
+            return solution;
         }
 
         public static long Mod(long a, long b)
