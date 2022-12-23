80b08c3bbeb0c05e288ba67253c79a725d3b9368
ipmaclean <iain.p.m.maclean@gmail.com>
2022-12-23 17:48:40 +0000

    Solved Day Twenty-three

--- AOC_2022/Day23/Day23InputHelper.cs
+++ AOC_2022/Day23/Day23InputHelper.cs
@@ -1,19 +1,31 @@
 ﻿namespace AOC_2022.Day23
 {
-    internal class Day23InputHelper : InputHelper<List<int>>
+    internal class Day23InputHelper : InputHelper<HashSet<Elf>>
     {
         public Day23InputHelper(string fileName) : base(fileName)
         {
         }
 
-        public override List<int> Parse()
+        public override HashSet<Elf> Parse()
         {
-            var output = new List<int>();
+            var output = new HashSet<Elf>();
+
             using (var sr = new StreamReader(InputPath))
             {
+                var yCoord = 0;
                 string ln;
                 while ((ln = sr.ReadLine()!) != null)
                 {
+                    var xCoord = 0;
+                    foreach (var tile in ln)
+                    {
+                        if (tile == '#')
+                        {
+                            output.Add(new Elf((xCoord, yCoord)));
+                        }
+                        xCoord++;
+                    }
+                    yCoord++;
                 }
             }
             return output;
---
+++ AOC_2022/Day23/Elf.cs
@@ -0,0 +1,14 @@
+﻿namespace AOC_2022.Day23
+{
+    public class Elf
+    {
+        public Elf((int X, int Y) coords)
+        {
+            Coords = coords;
+            Proposal = null;
+        }
+
+        public (int X, int Y) Coords { get; set; }
+        public (int X, int Y)? Proposal { get; set; }
+    }
+}
