5f56b951600ef5bb445285a1baee9f12b472f075
ipmaclean <iain.p.m.maclean@gmail.com>
2022-12-24 21:55:36 +0000

    Optimised Day Twenty-three. It now runs _much_ faster.

--- AOC_2022/Day23/Day23PuzzleManager.cs
+++ AOC_2022/Day23/Day23PuzzleManager.cs
@@ -4,8 +4,6 @@ namespace AOC_2022.Day23
 {
     public class Day23PuzzleManager : PuzzleManager
     {
-        //new protected const string INPUT_FILE_NAME = "test2.txt";
-
         private Day23InputHelper _inputHelper;
 
         public Day23PuzzleManager()
@@ -43,14 +41,15 @@ namespace AOC_2022.Day23
                 (1, 0),
                 (1, 1)
             };
+            var anyElfMoved = true;
             for (var i = 0; i < 10; i++)
             {
-                directionCheckIndex = SimulateRound(elves, directionCheckIndex, adjacentPositions).DirectionCheckIndex;
+                (elves, directionCheckIndex, anyElfMoved) = SimulateRound(elves, directionCheckIndex, adjacentPositions);
             }
-            var minElfX = elves.Min(elf => elf.Coords.X);
-            var minElfY = elves.Min(elf => elf.Coords.Y);
-            var maxElfX = elves.Max(elf => elf.Coords.X);
-            var maxElfY = elves.Max(elf => elf.Coords.Y);
+            var minElfX = elves.Keys.Min(elf => elf.X);
+            var minElfY = elves.Keys.Min(elf => elf.Y);
+            var maxElfX = elves.Keys.Max(elf => elf.X);
+            var maxElfY = elves.Keys.Max(elf => elf.Y);
 
             var solution = (maxElfX - minElfX + 1) * (maxElfY - minElfY + 1) - elves.Count;
             Console.WriteLine($"The solution to part one is '{solution}'.");
@@ -78,7 +77,7 @@ namespace AOC_2022.Day23
 
             while (anyElfMoved)
             {
-                (directionCheckIndex, anyElfMoved) = SimulateRound(elves, directionCheckIndex, adjacentPositions);
+                (elves, directionCheckIndex, anyElfMoved) = SimulateRound(elves, directionCheckIndex, adjacentPositions);
                 solution++;
             }
 
@@ -86,16 +85,19 @@ namespace AOC_2022.Day23
             return Task.CompletedTask;
         }
 
-        private (int DirectionCheckIndex, bool AnyElfMoved) SimulateRound(HashSet<Elf> elves, int directionCheckIndex, List<(int x, int y)> adjacentPositions)
+        private (Dictionary<(int X, int Y), (int X, int Y)> Elves, int DirectionCheckIndex, bool AnyElfMoved) SimulateRound(
+            Dictionary<(int X, int Y), (int X, int Y)> elves,
+            int directionCheckIndex,
+            List<(int x, int y)> adjacentPositions
+            )
         {
-            var anyElfMoved = false;
             // Half one
             foreach (var elf in elves)
             {
                 var hasAdjacentElf = false;
                 foreach (var adjacentPosition in adjacentPositions)
                 {
-                    if (elves.Any(e => e.Coords == (elf.Coords.X + adjacentPosition.x, elf.Coords.Y + adjacentPosition.y)))
+                    if (elves.ContainsKey((elf.Key.X + adjacentPosition.x, elf.Key.Y + adjacentPosition.y)))
                     {
                         hasAdjacentElf = true;
                         break;
@@ -103,56 +105,58 @@ namespace AOC_2022.Day23
                 }
                 if (!hasAdjacentElf)
                 {
+                    elves[elf.Key] = elf.Key;
                     continue;
                 }
-                FindProposal(elf, elves, directionCheckIndex);
+                FindProposal(elf.Key, elves, directionCheckIndex);
             }
 
             // Half two
+            var newElves = new Dictionary<(int X, int Y), (int X, int Y)>();
             foreach (var elf in elves)
             {
-                if (elf.Proposal is null)
+                if (!newElves.ContainsKey(elf.Value))
                 {
-                    continue;
+                    newElves.Add(elf.Value, (0, 0));
                 }
-                var elvesWithSameProposal = elves.Where(x => x.Proposal == elf.Proposal).ToList();
-                if (elvesWithSameProposal.Count > 1)
+                else
                 {
-                    foreach (var elfWithSameProposal in elvesWithSameProposal)
-                    {
-                        elfWithSameProposal.Proposal = null;
-                    }
-                    continue;
+                    // No more than two elves can propose the same space, and
+                    // they must come from opposite directions. Therefore, if
+                    // there is alreay an elf in the proposed space, you can
+                    // 'push' them back one space to take them back to their
+                    // original space.
+                    newElves.Remove(elf.Value);
+                    newElves.Add((2 * elf.Value.X - elf.Key.X, 2 * elf.Value.Y - elf.Key.Y), (0, 0));
+                    newElves.Add(elf.Key, (0, 0));
                 }
-                anyElfMoved = true;
-                elf.Coords = elf.Proposal.Value;
-                elf.Proposal = null;
             }
-
+            var anyElfMoved = elves.Keys.Any(x => !newElves.ContainsKey(x));
             directionCheckIndex = (directionCheckIndex + 1) % 4;
-            return (directionCheckIndex, anyElfMoved);
+            return (newElves, directionCheckIndex, anyElfMoved);
         }
 
         private void FindProposal(
-            Elf elf,
-            HashSet<Elf> elves,
+            (int X, int Y) elf,
+            Dictionary<(int X, int Y), (int X, int Y)> elves,
             int directionCheckIndex)
         {
             for (var i = 0; i < 4; i++)
             {
-                var vectorsToCheck = GetVectorsToCheck((directionCheckIndex + i) % 4);
                 var elfFree = true;
+                var vectorsToCheck = GetVectorsToCheck((directionCheckIndex + i) % 4);
                 foreach (var vector in vectorsToCheck)
                 {
-                    if (elves.Any(e => e.Coords == (elf.Coords.X + vector.x, elf.Coords.Y + vector.y)))
+                    if (elves.ContainsKey((elf.X + vector.x, elf.Y + vector.y)))
                     {
                         elfFree = false;
+                        elves[elf] = elf;
                         break;
                     }
                 }
                 if (elfFree)
                 {
-                    elf.Proposal = (elf.Coords.X + vectorsToCheck[1].x, elf.Coords.Y + vectorsToCheck[1].y);
+                    elves[elf] = (elf.X + vectorsToCheck[1].x, elf.Y + vectorsToCheck[1].y);
                     break;
                 }
             }
