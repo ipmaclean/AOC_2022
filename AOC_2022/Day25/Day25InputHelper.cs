e73b85d25a349f65480dc0abf0b35288bcb5b6b5
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2022-12-25 08:27:32 +0000

    Solved Day Twenty-five. (#28)

--- AOC_2022/Day25/Day25InputHelper.cs
+++ AOC_2022/Day25/Day25InputHelper.cs
@@ -1,19 +1,20 @@
 ﻿namespace AOC_2022.Day25
 {
-    internal class Day25InputHelper : InputHelper<List<int>>
+    internal class Day25InputHelper : InputHelper<List<string>>
     {
         public Day25InputHelper(string fileName) : base(fileName)
         {
         }
 
-        public override List<int> Parse()
+        public override List<string> Parse()
         {
-            var output = new List<int>();
+            var output = new List<string>();
             using (var sr = new StreamReader(InputPath))
             {
                 string ln;
                 while ((ln = sr.ReadLine()!) != null)
                 {
+                    output.Add(ln);
                 }
             }
             return output;
--- AOC_2022/Day25/Day25PuzzleManager.cs
+++ AOC_2022/Day25/Day25PuzzleManager.cs
@@ -1,10 +1,16 @@
-﻿namespace AOC_2022.Day25
+﻿using AOC_2022.Day22;
+using System.Text;
+
+namespace AOC_2022.Day25
 {
     public class Day25PuzzleManager : PuzzleManager
     {
+        public List<string> Input { get; set; }
+
         public Day25PuzzleManager()
         {
             var inputHelper = new Day25InputHelper(INPUT_FILE_NAME);
+            Input = inputHelper.Parse();
         }
         public override Task SolveBothParts()
         {
@@ -16,15 +22,77 @@
 
         public override Task SolvePartOne()
         {
-            var solution = 0;
-            Console.WriteLine($"The solution to part one is '{solution}'.");
+            var sum = 0d;
+            foreach (var snafu in Input)
+            {
+                sum += SnafuToDecimal(snafu);
+            }
+            Console.WriteLine($"The solution to part one is '{DecimalToSnafu(sum)}'.");
             return Task.CompletedTask;
         }
 
+        private double SnafuToDecimal(string snafu)
+        {
+            var sum = 0d;
+            for (var i = snafu.Length - 1; i >= 0; i--)
+            {
+                sum += SnafuDigitToDecimal(snafu[i]) * Math.Pow(5, (double)(snafu.Length - 1 - i));
+            }
+            return sum;
+        }
+
+        private string DecimalToSnafu(double decNumber)
+        {
+            var sb = new StringBuilder();
+            // Find the largest power of 5 that when doubled
+            // is greater than or equal to the decimal.
+            var largestPower = 0;
+            while (2 * Math.Pow(5, largestPower) < decNumber)
+            {
+                largestPower++;
+            }
+            for (var i = largestPower; i >= 0; i--)
+            {
+                var closestMultiplierToZero = 0d;
+                var closestValueToZero = double.MaxValue;
+                for (var multiplier = 2; multiplier >= -2; multiplier--)
+                {
+                    var remainder = decNumber - multiplier * Math.Pow(5, (double)i);
+                    if (Math.Abs(remainder) < Math.Abs(closestValueToZero))
+                    {
+                        closestValueToZero = remainder;
+                        closestMultiplierToZero = multiplier;
+                    }
+                }
+                decNumber = closestValueToZero;
+                sb.Append(DecimalDigitToSnafu((int)closestMultiplierToZero));
+            }
+            return sb.ToString();
+        }
+
+        private static double SnafuDigitToDecimal(char digit) => digit switch
+        {
+            '2' => 2,
+            '1' => 1,
+            '0' => 0,
+            '-' => -1,
+            '=' => -2,
+            _ => throw new ArgumentOutOfRangeException($"Unexpected digit value: {digit}"),
+        };
+
+        private static char DecimalDigitToSnafu(int digit) => digit switch
+        {
+            2 => '2',
+            1 => '1',
+            0 => '0',
+            -1 => '-',
+            -2 => '=',
+            _ => throw new ArgumentOutOfRangeException($"Unexpected digit value: {digit}"),
+        };
+
         public override Task SolvePartTwo()
         {
-            var solution = 0;
-            Console.WriteLine($"The solution to part two is '{solution}'.");
+            Console.WriteLine($"Well done, you've saved Christmas!");
             return Task.CompletedTask;
         }
     }
